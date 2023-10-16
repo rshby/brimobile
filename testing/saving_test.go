1f6b243aaabcc5b0379116c73d0233173cec7421
Reo Sahobby <reoshby@gmail.com>
2023-10-16 13:39:11 +0700

    refactor(test): add unit test

--- testing/saving_test.go
+++ testing/saving_test.go
@@ -38,6 +38,31 @@ func TestInsertSaving(t *testing.T) {
 		assert.NotNil(t, res)
 		assert.Equal(t, entity.AccountNumber, res.AccountNumber)
 	})
+
+	// test insert saving error
+	t.Run("test insert saving failed", func(t *testing.T) {
+		savingRepo := mck.NewSavingRepositoryMock()
+		brinjournalRepo := mck.NewBrinJournalMock()
+		savingService := service.NewSavingService(savingRepo, brinjournalRepo)
+
+		// mock set method insert in SavingRepository -> return error
+		errorMessage := "gagal insert"
+		savingRepo.Mock.On("Insert", mock.Anything, mock.Anything).Return(nil, errors.New(errorMessage)).Times(1)
+
+		// run method Insert in SavingService
+		res, err := savingService.Insert(context.Background(), model.InsertSavingRequest{
+			AccountNumber: "0452001",
+			ShortName:     "Reo Sahobby",
+			Cbal:          "100.0",
+		})
+
+		// validate test
+		assert.Nil(t, res)
+		assert.NotNil(t, err)
+		assert.Error(t, err)
+		assert.Equal(t, errorMessage, err.Error())
+		savingRepo.Mock.AssertExpectations(t)
+	})
 }
 
 // test get by account_number
